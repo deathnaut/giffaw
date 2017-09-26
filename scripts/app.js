c9f0bb4f7c6a02e899f67df42bdb8fe0110c3d89
Karla Moreno <moreno_karla@live.com>
2017-09-26 16:28:59 -0500

    giffaw shows first 25 gifs tagged "cats", note: search not yet functional

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,51 @@
-$(document).on("ready", function(){
+$(function(){
+console.log('da script runnin...');
+
+$.ajax({
+  method: 'GET',
+  url: 'http://api.giphy.com/v1/gifs/search?q=cats&api_key=dc6zaTOxFJmzC',
+  dataType: 'json',
+  success: onSuccess,
+  error: onError
+})
+
+function onSuccess(responseData){
+  var test = responseData.data[0].slug;
+  getGifUrl(responseData);
+}
+
+
+function onError(){
+  alert('error: fix yer shit brah');
+}
+
+function getGifUrl(responseData){
+  var images = [];
+  // loop up to 25 times to get first 25 gif links
+  for (var i = 0; i < 25; i++) {
+    var imgURL = responseData.data[i].images.fixed_width.url;
+    images.push(imgURL);
+  }
+  generateHTML(images);
+}
+
+
+function generateHTML(images){
+  console.log('generateHTML function start');
+  // method 1
+  var imagesHTML = images.map(function(img) {
+    var imgHTML = '<img src="' + img +'">';
+    $('.gif-gallery').append(imgHTML);
+    return imgHTML;
+  });
+
+  // for each!!!!!
+  // method 2
+
+
+  console.log(imagesHTML.length);
+  console.log(imagesHTML);
+  console.log('generateHTML function end');
+}
 
 });
