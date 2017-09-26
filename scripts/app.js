e58f84563f367347f3939d5ed019e249ced6b899
Karla Moreno <moreno_karla@live.com>
2017-09-26 18:19:06 -0500

    solved up to 5!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,24 @@
 $(function(){
 console.log('da script runnin...');
 
-$.ajax({
-  method: 'GET',
-  url: 'http://api.giphy.com/v1/gifs/search?q=cats&api_key=dc6zaTOxFJmzC',
-  dataType: 'json',
-  success: onSuccess,
-  error: onError
-})
+showGifs();
+
+$('form').submit(function(event) {
+  event.preventDefault();
+  $('img').remove();
+  showGifs();
+});
+
+function showGifs(){
+  $.ajax({
+    method: 'GET',
+    url: 'http://api.giphy.com/v1/gifs/search?',
+    data: $('form').serialize(),
+    dataType: 'json',
+    success: onSuccess,
+    error: onError
+  })
+}
 
 function onSuccess(responseData){
   var test = responseData.data[0].slug;
@@ -16,7 +27,7 @@ function onSuccess(responseData){
 
 
 function onError(){
-  alert('error: fix yer shit brah');
+  console.log('error: SEEK & DESTROY');
 }
 
 function getGifUrl(responseData){
@@ -32,20 +43,24 @@ function getGifUrl(responseData){
 
 function generateHTML(images){
   console.log('generateHTML function start');
-  // method 1
+  // option 1
   var imagesHTML = images.map(function(img) {
     var imgHTML = '<img src="' + img +'">';
     $('.gif-gallery').append(imgHTML);
     return imgHTML;
   });
+  // ------------------------------------------------
 
   // for each!!!!!
-  // method 2
+  // option 2
+  // images.forEach(function(img){
+  //
+  // })
 
 
-  console.log(imagesHTML.length);
-  console.log(imagesHTML);
-  console.log('generateHTML function end');
+  // console.log(imagesHTML.length);
+  // console.log(imagesHTML);
+  // console.log('generateHTML function end');
 }
 
 });
